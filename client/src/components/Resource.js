fbf875b3d9c6f9487433e3eb6be0daabcc7c8c7e
LarissaMorrell <larissa@larissamorrell.com>
2018-05-07 17:13:33 -0400

    Reusable resource component

--- client/src/components/Resource.js
+++ client/src/components/Resource.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { sunPlant } from "../actions/resources";
-import { waterPlant } from "../actions/resources";
 
 const resourceImg = {
   sun: "sun.png",
@@ -11,15 +9,13 @@ const resourceImg = {
 export class Resource extends Component {
   render() {
     let resource = this.props.resource;
-    let functName = this.props.resource + "Plant";
-    console.log(window[functName]());
     return (
       <div className={resource}>
         <input
           type="image"
           alt={resource}
           src={`/media/${resourceImg[resource]}`}
-          onClick={() => this.props.dispatch(window[functName]())}
+          onClick={() => this.props.onClick}
         />
       </div>
     );
--- client/src/components/ResourcesContainer.js
+++ client/src/components/ResourcesContainer.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Resource from "./Resource";
 import Water from "./Water";
 import Sun from "./Sun";
 
-export default class ResourcesContainer extends Component {
+import { sunPlant } from "../actions/resources";
+import { waterPlant } from "../actions/resources";
+
+export class ResourcesContainer extends Component {
   render() {
     return (
       <div className="ResourcesContainer">
-        <Resource resource="sun" />
+        <Resource resource="sun" onClick={this.props.dispatch(sunPlant())} />
         <Sun />
         <Water />
       </div>
     );
   }
 }
+export default connect()(ResourcesContainer);
