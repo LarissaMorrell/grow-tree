17c824b62c00341c6fa8f0b6dec3a511cf4e8874
LarissaMorrell <larissa@larissamorrell.com>
2018-05-07 06:28:52 -0400

    (added) treeTransform action/reducer (bug) triggering treeTransform produces proxy/CORS error

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
-app.get("/api/helloworld", (req, res) => {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/api", (req, res) => {
   res.send({ greeting: "Hello World!" });
 });
 
