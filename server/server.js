ecfc8f79b405af32712fed90acd3883affd37f4d
LarissaMorrell <larissa@larissamorrell.com>
2018-05-07 06:47:52 -0400

    (fixed) treeTransform req CORS error

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+//Enable CORS on the server for development since
+//since we need to use proxy with create-react-app
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
