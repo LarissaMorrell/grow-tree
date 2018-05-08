375c8afa2a29bc94be4528c95ecbfa6f39225eb8
LarissaMorrell <larissa@larissamorrell.com>
2018-05-08 11:06:19 -0400

    (added) test for mounting Resource componenents

--- client/src/__tests__/ResourcesContainer.js
+++ client/src/__tests__/ResourcesContainer.js
@@ -6,29 +6,7 @@ describe("<ResourcesContainer />", () => {
   it("renders without crashing", () => {
     shallow(<ResourcesContainer />);
   });
-  // it("should fire onClick", () => {
-  //   let callback = jest.fn();
-  //   let wrapper = mount(
-  //     <ResourcesContainer resource="water" onClick={callback} />
-  //   );
-  //   wrapper.simulate("click");
-  //   expect(callback).toHaveBeenCalled();
-  // });
-  //
-  // it("should mount in a full DOM", function() {
-  //   expect(
-  //     mount(<ResourcesContainer resource="water" onClick={() => {}} />).find(".ResourcesContainer")
-  //       .length
-  //   ).toBe(1);
-  // });
-
-  // it('Should switch to editing when the add button is clicked', () => {
-  //     const wrapper = shallow(<ResourcesContainer />);
-  //     wrapper.simulate('click');
-  //     expect(wrapper.state('editing')).toEqual(true);
-  // });
-
-  // it("should render without throwing an error", function() {
-  //   expect(shallow(<ResourcesContainer />).contains(<input />)).toBe(true);
-  // });
+  it("should mount on DOM with 2 <Resource/>", function() {
+    expect(mount(<ResourcesContainer />).find(".Resource").length).toBe(2);
+  });
 });
